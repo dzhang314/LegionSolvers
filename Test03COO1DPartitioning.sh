4ee17997caa09725e9d7f06036412dc28a919c07
David K. Zhang <dzhang314@gmail.com>
2022-01-27 04:31:23 -0800

    add new unit test template

---
+++ Test03COO1DPartitioning.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+
+module load cuda
+
+export CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK
+export GASNET_BACKTRACE=1
+export LEGION_BACKTRACE=1
+
+/home/dkzhang/LegionSolvers/build_master_debug/Test03COO1DPartitioning \
+    -lg:warn -lg:leaks -ll:ocpu 1 -ll:onuma 0 -ll:gpu 1
