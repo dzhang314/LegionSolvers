a4884797488ed494ca110431026789b781824b1b
David K. Zhang <dzhang314@gmail.com>
2022-01-27 17:01:56 -0800

    temporarily disable LegionSolversMapper for debugging

--- src/LegionSolversMapper.cpp
+++ src/LegionSolversMapper.cpp
@@ -40,31 +40,31 @@ void LegionSolvers::LegionSolversMapper::slice_task(
     const SliceTaskInput& input,
     SliceTaskOutput& output
 ) {
-    if (is_task(task.task_id, COO_MATVEC_TASK_BLOCK_ID)) {
-        const Legion::DomainPoint lo = input.domain.lo();
-        const Legion::DomainPoint hi = input.domain.hi();
-        for (Legion::coord_t i = lo[2]; i <= hi[2]; i++) {
-            const Legion::Rect<3> rect{
-                Legion::Point<3>{lo[0], lo[1], i},
-                Legion::Point<3>{hi[0], hi[1], i}
-            };
-            output.slices.emplace_back(
-                input.domain.intersection(Legion::Domain{rect}),
-                gpus[address_spaces[i % address_spaces.size()]],
-                false, // do not recursively call slice_task
-                false // TODO: should this be stealable?
-            );
-        }
-        // for (auto &slice : output.slices) {
-        //     std::cout << "Assigning " << slice.domain << " to rank "
-        //                 << slice.proc.address_space() << std::endl;
-        // }
-        // output.verify_correctness = true;
-    } else {
+    // if (is_task(task.task_id, COO_MATVEC_TASK_BLOCK_ID)) {
+    //     const Legion::DomainPoint lo = input.domain.lo();
+    //     const Legion::DomainPoint hi = input.domain.hi();
+    //     for (Legion::coord_t i = lo[2]; i <= hi[2]; i++) {
+    //         const Legion::Rect<3> rect{
+    //             Legion::Point<3>{lo[0], lo[1], i},
+    //             Legion::Point<3>{hi[0], hi[1], i}
+    //         };
+    //         output.slices.emplace_back(
+    //             input.domain.intersection(Legion::Domain{rect}),
+    //             gpus[address_spaces[i % address_spaces.size()]],
+    //             false, // do not recursively call slice_task
+    //             false // TODO: should this be stealable?
+    //         );
+    //     }
+    //     // for (auto &slice : output.slices) {
+    //     //     std::cout << "Assigning " << slice.domain << " to rank "
+    //     //                 << slice.proc.address_space() << std::endl;
+    //     // }
+    //     // output.verify_correctness = true;
+    // } else {
         Legion::Mapping::DefaultMapper::slice_task(
             ctx, task, input, output
         );
-    }
+    // }
 }
 
 
