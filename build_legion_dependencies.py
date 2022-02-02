5fa12a1ef6193114c93b531fe00e949d78dc0b4f
David K. Zhang <dzhang314@gmail.com>
2022-02-02 13:01:43 -0800

    remove Kokkos_USE_CXX_EXTENSIONS flag, as this appears to do nothing

--- build_legion_dependencies.py
+++ build_legion_dependencies.py
@@ -138,7 +138,6 @@ def main():
             "Kokkos_ENABLE_OPENMP": True,
             "Kokkos_ENABLE_CUDA": True,
             "Kokkos_ENABLE_CUDA_LAMBDA": True,
-            "Kokkos_USE_CXX_EXTENSIONS": True,
             "Kokkos_ENABLE_TESTS": True,
         }, test=(MACHINE != Machines.SAPLING), install=True)
         # one test is known to fail on Sapling
@@ -164,7 +163,6 @@ def main():
             "Kokkos_ENABLE_OPENMP": True,
             "Kokkos_ENABLE_CUDA": True,
             "Kokkos_ENABLE_CUDA_LAMBDA": True,
-            "Kokkos_USE_CXX_EXTENSIONS": True,
             "Kokkos_ENABLE_TESTS": True,
         }
         # Kokkos 3.0.00 does not auto-detect compute capability
