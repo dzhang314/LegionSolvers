8ceae6a6a4c938124182eb9b56cf6110532b739f
David K. Zhang <dzhang314@gmail.com>
2022-09-26 13:03:10 -0700

    begin reformatting files

--- src/LibraryOptions.hpp
+++ src/LibraryOptions.hpp
@@ -1,58 +1,56 @@
-#ifndef LEGION_SOLVERS_LIBRARY_OPTIONS_HPP
-#define LEGION_SOLVERS_LIBRARY_OPTIONS_HPP
+#ifndef LEGION_SOLVERS_LIBRARY_OPTIONS_HPP_INCLUDED
+#define LEGION_SOLVERS_LIBRARY_OPTIONS_HPP_INCLUDED
 
 #include <legion.h>
 
 #include "MetaprogrammingUtilities.hpp"
 
-
 namespace LegionSolvers {
 
 
-    #ifndef LEGION_SOLVERS_MAPPER_ID
-    constexpr Legion::MapperID
-    LEGION_SOLVERS_MAPPER_ID = 1'000;
-    #endif
+#ifndef LEGION_SOLVERS_MAPPER_ID
+constexpr Legion::MapperID
+LEGION_SOLVERS_MAPPER_ID = 1'000;
+#endif
 
 
-    #ifndef LEGION_SOLVERS_TASK_ID_ORIGIN
-    constexpr Legion::TaskID
-    LEGION_SOLVERS_TASK_ID_ORIGIN = 1'000'000;
-    #endif
+#ifndef LEGION_SOLVERS_TASK_ID_ORIGIN
+constexpr Legion::TaskID
+LEGION_SOLVERS_TASK_ID_ORIGIN = 1'000'000;
+#endif
 
 
-    #ifndef LEGION_SOLVERS_PROJECTION_ID_ORIGIN
-    constexpr Legion::ProjectionID
-    LEGION_SOLVERS_PROJECTION_ID_ORIGIN = 10'000;
-    #endif
+#ifndef LEGION_SOLVERS_PROJECTION_ID_ORIGIN
+constexpr Legion::ProjectionID
+LEGION_SOLVERS_PROJECTION_ID_ORIGIN = 10'000;
+#endif
 
 
-    #ifndef LEGION_SOLVERS_MAX_DIM
-    constexpr int
-    LEGION_SOLVERS_MAX_DIM = 3;
-    #endif
+#ifndef LEGION_SOLVERS_MAX_DIM
+constexpr int
+LEGION_SOLVERS_MAX_DIM = 3;
+#endif
 
 
-    static_assert(LEGION_MAX_DIM >= 3,
-                  "LegionSolvers requires Legion to be compiled with the "
-                  "preprocessor macro LEGION_MAX_DIM set to 3 or higher");
-    static_assert(LEGION_SOLVERS_MAX_DIM <= LEGION_MAX_DIM,
-                  "Legion was not compiled with LEGION_MAX_DIM large enough "
-                  "to support the specified value for LEGION_SOLVERS_MAX_DIM");
+static_assert(LEGION_MAX_DIM >= 3,
+                "LegionSolvers requires Legion to be compiled with the"
+                " preprocessor macro LEGION_MAX_DIM set to 3 or higher.");
+static_assert(LEGION_SOLVERS_MAX_DIM <= LEGION_MAX_DIM,
+                "Legion was not compiled with LEGION_MAX_DIM large enough to"
+                " support the specified value for LEGION_SOLVERS_MAX_DIM.");
 
 
-    using LEGION_SOLVERS_SUPPORTED_INDEX_TYPES = TypeList<int, unsigned, long long>;
-    using LEGION_SOLVERS_SUPPORTED_ENTRY_TYPES = TypeList<float, double>;
+using LEGION_SOLVERS_SUPPORTED_INDEX_TYPES = TypeList<int, unsigned, long long>;
+using LEGION_SOLVERS_SUPPORTED_ENTRY_TYPES = TypeList<float, double>;
 
 
-    #ifdef NDEBUG
-        constexpr bool LEGION_SOLVERS_CHECK_BOUNDS = false;
-    #else
-        constexpr bool LEGION_SOLVERS_CHECK_BOUNDS = true;
-    #endif
+#ifdef NDEBUG
+    constexpr bool LEGION_SOLVERS_CHECK_BOUNDS = false;
+#else
+    constexpr bool LEGION_SOLVERS_CHECK_BOUNDS = true;
+#endif
 
 
 } // namespace LegionSolvers
 
-
-#endif // LEGION_SOLVERS_LIBRARY_OPTIONS_HPP
+#endif // LEGION_SOLVERS_LIBRARY_OPTIONS_HPP_INCLUDED
--- src/MetaprogrammingUtilities.hpp
+++ src/MetaprogrammingUtilities.hpp
@@ -1,87 +1,85 @@
-#ifndef LEGION_SOLVERS_METAPROGRAMMING_UTILITIES_HPP
-#define LEGION_SOLVERS_METAPROGRAMMING_UTILITIES_HPP
+#ifndef LEGION_SOLVERS_METAPROGRAMMING_UTILITIES_HPP_INCLUDED
+#define LEGION_SOLVERS_METAPROGRAMMING_UTILITIES_HPP_INCLUDED
 
 #include <string>
-
 
 namespace LegionSolvers {
 
 
-    template <typename... TS>
-    struct TypeList {};
+template <typename... TS>
+struct TypeList {};
 
 
-    template <int... NS>
-    struct IntList {};
+template <int... NS>
+struct IntList {};
 
 
-    template <typename T>
-    struct ToString;
+template <typename T>
+struct ToString;
 
-    template <>
-    struct ToString<IntList<>> {
-        static std::string value() { return std::string{""}; }
-    };
+template <>
+struct ToString<IntList<>> {
+    static std::string value() { return std::string{""}; }
+};
 
-    template <int N, int... NS>
-    struct ToString<IntList<N, NS...>> {
-        static std::string value() {
-            return std::string{"_"} + std::to_string(N) +
-                   ToString<IntList<NS...>>::value();
-        }
-    };
-
-
-    template <typename L>
-    struct ListLength;
-
-    template <>
-    struct ListLength<IntList<>> {
-        static constexpr int value = 0;
-    };
-
-    template <int N, int... NS>
-    struct ListLength<IntList<N, NS...>> {
-        static constexpr int value = 1 + ListLength<IntList<NS...>>::value;
-    };
-
-    template <>
-    struct ListLength<TypeList<>> {
-        static constexpr int value = 0;
-    };
-
-    template <typename T, typename... TS>
-    struct ListLength<TypeList<T, TS...>> {
-        static constexpr int value = 1 + ListLength<TypeList<TS...>>::value;
-    };
+template <int N, int... NS>
+struct ToString<IntList<N, NS...>> {
+    static std::string value() {
+        return std::string{"_"} + std::to_string(N)
+                                + ToString<IntList<NS...>>::value();
+    }
+};
 
 
-    template <typename L, typename T>
-    struct ListIndex;
+template <typename L>
+struct ListLength;
 
-    template <typename L, typename... LS, typename T>
-    struct ListIndex<TypeList<L, LS...>, T> {
-        static constexpr int value = 1 + ListIndex<TypeList<LS...>, T>::value;
-    };
+template <>
+struct ListLength<IntList<>> {
+    static constexpr int value = 0;
+};
 
-    template <typename L, typename... LS>
-    struct ListIndex<TypeList<L, LS...>, L> {
-        static constexpr int value = 0;
-    };
+template <int N, int... NS>
+struct ListLength<IntList<N, NS...>> {
+    static constexpr int value = 1 + ListLength<IntList<NS...>>::value;
+};
+
+template <>
+struct ListLength<TypeList<>> {
+    static constexpr int value = 0;
+};
+
+template <typename T, typename... TS>
+struct ListLength<TypeList<T, TS...>> {
+    static constexpr int value = 1 + ListLength<TypeList<TS...>>::value;
+};
 
 
-    template <typename T, int N>
-    struct NestedPointer {
-        typedef typename NestedPointer<T, N - 1>::type *type;
-    };
+template <typename L, typename T>
+struct ListIndex;
 
-    template <typename T>
-    struct NestedPointer<T, 0> {
-        typedef T type;
-    };
+template <typename L, typename... LS, typename T>
+struct ListIndex<TypeList<L, LS...>, T> {
+    static constexpr int value = 1 + ListIndex<TypeList<LS...>, T>::value;
+};
+
+template <typename L, typename... LS>
+struct ListIndex<TypeList<L, LS...>, L> {
+    static constexpr int value = 0;
+};
+
+
+template <typename T, int N>
+struct NestedPointer {
+    typedef typename NestedPointer<T, N - 1>::type *type;
+};
+
+template <typename T>
+struct NestedPointer<T, 0> {
+    typedef T type;
+};
 
 
 } // namespace LegionSolvers
 
-
-#endif // LEGION_SOLVERS_METAPROGRAMMING_UTILITIES_HPP
+#endif // LEGION_SOLVERS_METAPROGRAMMING_UTILITIES_HPP_INCLUDED
