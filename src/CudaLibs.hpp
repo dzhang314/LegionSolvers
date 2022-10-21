5851c063f91cd16f4514bf401e32de5eb5a2daa1
Rohan Yadav <rohany@alumni.cmu.edu>
2022-10-20 20:44:49 -0700

    Add cuBLAS to the CudaLibs (#7)
    * src: initial version of adding cublas to the CudaLibs
    
    * src: fix indentation in CudaLibs.hpp

--- src/CudaLibs.hpp
+++ src/CudaLibs.hpp
@@ -4,6 +4,7 @@
 #include "TaskIDs.hpp"
 
 #include <assert.h>
+#include <cublas_v2.h>
 #include <cuda_runtime.h>
 #include <cusparse.h>
 #include <legion.h>
@@ -18,6 +19,12 @@
     do {                                                                       \
         CHECK_CUDA(cudaStreamSynchronize(stream));                             \
         CHECK_CUDA(cudaPeekAtLastError());                                     \
+    } while (false)
+
+#define CHECK_CUBLAS(expr)                                                     \
+    do {                                                                       \
+        cublasStatus_t __result__ = (expr);                                    \
+        check_cublas(__result__, __FILE__, __LINE__);                          \
     } while (false)
 
 #define CHECK_CUSPARSE(expr)                                                   \
@@ -38,13 +45,21 @@ __host__ inline void check_cuda(cudaError_t error, const char *file, int line) {
             file,
             line
         );
-#ifndef NDEBUG
         assert(false);
-#else
-        exit(error);
-#endif
     }
 }
+
+__host__ inline void check_cublas(cublasStatus_t status, const char* file, int line) {
+  if (status != CUBLAS_STATUS_SUCCESS) {
+    fprintf(stderr,
+            "cuBLAS failure with error code %d in file %s at line %d\n",
+            status,
+            file,
+            line);
+    assert(false);
+  }
+}
+
 
 __host__ inline void
 checkCuSparse(cusparseStatus_t status, const char *file, int line) {
@@ -86,8 +101,10 @@ struct StreamView {
 
 // Return a cached stream for the current GPU.
 StreamView get_cached_stream();
-// Method to get the CUSPARSE handle associated with the executing GPU.
+// Method to get the cuSPARSE handle associated with the executing GPU.
 cusparseHandle_t get_cusparse();
+// Method to get the cuBLAS handle associated with the executing GPU.
+cublasHandle_t get_cublas();
 
 // CUDALibraries is a struct that manages handles on libraries
 // like cuSPARSE and (in the future) cuBLAS, as well as what
@@ -102,10 +119,12 @@ struct CUDALibraries {
     CUDALibraries &operator=(const CUDALibraries &rhs) = delete;
 
   public:
+    cublasHandle_t get_cublas();
     cusparseHandle_t get_cusparse();
     cudaStream_t get_stream();
 
   private:
+    cublasHandle_t cublas_;
     cusparseHandle_t cusparse_;
     cudaStream_t stream_;
 };
