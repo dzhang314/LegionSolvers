be9cd2349bdfcae9b6e04632ed443ed44ec587b9
Rohan Yadav <rohany@alumni.cmu.edu>
2022-10-20 20:15:25 -0700

    src: fix fallout from changing preregistration hooks

--- src/Test01ScalarOperations.cpp
+++ src/Test01ScalarOperations.cpp
@@ -35,7 +35,10 @@ int main(int argc, char **argv) {
     using LegionSolvers::TaskFlags;
     LegionSolvers::initialize(false);
     LegionSolvers::preregister_task<top_level_task>(
-        TOP_LEVEL_TASK_ID, "top_level", TaskFlags::REPLICABLE | TaskFlags::INNER
+        TOP_LEVEL_TASK_ID,
+        "top_level",
+        TaskFlags::REPLICABLE | TaskFlags::INNER,
+        Legion::Processor::LOC_PROC
     );
     Legion::Runtime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
     return Legion::Runtime::start(argc, argv);
