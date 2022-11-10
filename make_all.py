48c0fc7af9eb37a8f50bd983f94d06b3eddcdc93
David K. Zhang <dzhang314@gmail.com>
2022-11-10 13:57:41 -0800

    modify make_all to only build selected configs

--- make_all.py
+++ make_all.py
@@ -11,6 +11,16 @@ from build_legion_variants import (
 ################################################################################
 
 
+DESIRED_VARIANTS = [
+    "master_gex_nocuda_nokokkos_debug",
+    "master_gex_cuda_nokokkos_debug",
+    "cr_gex_nocuda_nokokkos_debug",
+    "cr_gex_cuda_nokokkos_debug",
+    "coll_gex_nocuda_nokokkos_debug",
+    "coll_gex_cuda_nokokkos_debug",
+]
+
+
 def main():
     for dir_name, branch_name in LEGION_BRANCHES:
         for build_type in BUILD_TYPES:
@@ -19,15 +29,14 @@ def main():
                     for kokkos_tag, use_kokkos in KOKKOS_TYPES:
                         if use_kokkos and not use_cuda:
                             continue
-                        build_name = os.path.join(
-                            "build",
-                            join(
-                                dir_name, network_tag, cuda_tag,
-                                kokkos_tag, build_type.lower()
-                            )
+                        build_name = join(
+                            dir_name, network_tag, cuda_tag,
+                            kokkos_tag, build_type.lower()
                         )
-                        with pushd(build_name):
-                            run("cmake", "--build", ".", "--parallel", "20")
+                        if build_name in DESIRED_VARIANTS:
+                            with pushd(os.path.join("build", build_name)):
+                                run("cmake", "--build", ".", "--parallel", "20")
+
 
 ################################################################################
 
