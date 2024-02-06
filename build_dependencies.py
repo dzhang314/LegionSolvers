db1efecb9ebdc874e6e901abc9b2093fcddf9773
David K. Zhang <dzhang314@gmail.com>
2024-02-06 14:18:17 -0800

    update build_dependencies for Kokkos 4.2

--- build_dependencies.py
+++ build_dependencies.py
@@ -19,11 +19,11 @@ from build_utilities import (
 
 GASNET_GIT_URL: str = "https://github.com/StanfordLegion/gasnet.git"
 KOKKOS_GIT_URL: str = "https://github.com/kokkos/kokkos.git"
-KOKKOS_4_1_URL: str = "https://github.com/kokkos/kokkos/archive/refs/tags/4.1.00.zip"
+KOKKOS_4_2_URL: str = "https://github.com/kokkos/kokkos/archive/refs/tags/4.2.00.zip"
 
 
-_KOKKOS_CUDA_LIB_NAME: str = "kokkos-4.1.00-cuda"
-_KOKKOS_NOCUDA_LIB_NAME: str = "kokkos-4.1.00-nocuda"
+_KOKKOS_CUDA_LIB_NAME: str = "kokkos-4.2.00-cuda"
+_KOKKOS_NOCUDA_LIB_NAME: str = "kokkos-4.2.00-nocuda"
 
 
 KOKKOS_CUDA_CMAKE_PATH: str = _os.path.join(
@@ -56,11 +56,11 @@ def main():
             run("make", "CONDUIT=ibv")  # TODO: machine-to-conduit mapping
 
     _os.chdir(SCRATCH_DIR)
-    remove_directory("kokkos-4.1.00")
-    download(KOKKOS_4_1_URL)
-    run("unzip", "4.1.00.zip")
+    remove_directory("kokkos-4.2.00")
+    download(KOKKOS_4_2_URL)
+    run("unzip", "4.2.00.zip")
 
-    with change_directory("kokkos-4.1.00"):
+    with change_directory("kokkos-4.2.00"):
         defines_cuda: CMakeDefines = {
             "CMAKE_CXX_STANDARD": 17,
             "CMAKE_BUILD_TYPE": "Release",
