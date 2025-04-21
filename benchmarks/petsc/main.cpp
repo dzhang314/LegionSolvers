9f778e9f9cde1ea07bf6d2427dc9c4b071d77032
Rohan Yadav <rohany@g0003.stanford.edu>
2025-04-21 14:24:15 -0700

    missing newline

--- benchmarks/petsc/main.cpp
+++ benchmarks/petsc/main.cpp
@@ -299,7 +299,7 @@ int main(int argc, char **argv) {
     PetscCall(KSPGetIterationNumber(ksp, &num_iterations));
     PetscCall(PetscPrintf(
         PETSC_COMM_WORLD,
-        "Performed %d iterations.\nTook %f ms per iteration.\nThroughput %f it/s.",
+        "Performed %d iterations.\nTook %f ms per iteration.\nThroughput %f it/s.\n",
         num_iterations,
         1000.0 * elapsed / num_iterations,
 	num_iterations / elapsed
